9312e2edd0d0d5fd369aa3513d2a41e701af232a
Gordon_Heart <Gordon_Heart@kciscs.org.cn>
2024-11-16 20:33:10 +0800

    Initial commit

---
+++ frontend/src/api.js
@@ -0,0 +1,30 @@
+import axios from "axios";
+import { ACCESS_TOKEN } from "./constants";
+
+const apiUrl = "/choreo-apis/awbo/backend/rest-api-be2/v1.0";
+
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : apiUrl,
+  
+
+  headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+  },
+});
+
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem(ACCESS_TOKEN);
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    console.error("Request Interceptor Error:", error);
+    return Promise.reject(error);
+  }
+);
+
+export default api;
